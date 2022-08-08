bde4bc61f267844af58ff984f00c758bcb1f0aa0
marcinszyb <marcinszyb@gmail.com>
2022-08-08 17:12:35 +0200

    small login fixes + header added

--- src/main/java/pl/mszyb/med_facility/controller/HomepageController.java
+++ src/main/java/pl/mszyb/med_facility/controller/HomepageController.java
@@ -2,7 +2,6 @@ package pl.mszyb.med_facility.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
