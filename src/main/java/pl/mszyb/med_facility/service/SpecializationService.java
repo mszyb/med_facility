5c750cc0d408ce9eaa8c735ae91e0f94b835364c
mszyb <marcinszyb@gmail.com>
2022-08-09 21:21:51 +0200

    specialization listing

---
+++ src/main/java/pl/mszyb/med_facility/service/SpecializationService.java
@@ -0,0 +1,11 @@
+package pl.mszyb.med_facility.service;
+
+import pl.mszyb.med_facility.entity.Specialization;
+
+import java.util.List;
+
+public class SpecializationService {
+    public List<Specialization> findAll(){
+
+    }
+}
