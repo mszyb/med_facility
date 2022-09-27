5156fcdc1f1a743a9a34041a2ce40a918d1a9dc1
mszyb <marcinszyb@gmail.com>
2022-09-27 12:22:13 +0200

    RegistrationController code cleaning

--- src/main/java/pl/mszyb/med_facility/service/ConfirmationTokenService.java
+++ src/main/java/pl/mszyb/med_facility/service/ConfirmationTokenService.java
@@ -3,12 +3,17 @@ package pl.mszyb.med_facility.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.mszyb.med_facility.entity.ConfirmationToken;
+import pl.mszyb.med_facility.entity.User;
 import pl.mszyb.med_facility.repository.ConfirmationTokenRepository;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenService {
     private final ConfirmationTokenRepository confirmationTokenRepository;
+    private final UserService userService;
+    private final RoleService roleService;
 
     public void save(ConfirmationToken confirmationToken) {
         confirmationTokenRepository.save(confirmationToken);
@@ -20,5 +25,15 @@ public class ConfirmationTokenService {
 
     public void deleteByConfirmationToken(String confirmationToken){
         confirmationTokenRepository.deleteByConfirmationToken(confirmationToken);
+    }
+
+    public void confirmUserEmail(ConfirmationToken tokenFromDb){
+        Long userId = tokenFromDb.getUser().getId();
+        Optional<User> userOptional = userService.findById(userId);
+        userOptional.ifPresent(user -> {
+            user.setVerified(true);
+            user.setRole(roleService.findRoleByName("ROLE_USER"));
+            userService.update(user);
+        });
     }
 }
--- src/main/java/pl/mszyb/med_facility/service/EmailSenderService.java
+++ src/main/java/pl/mszyb/med_facility/service/EmailSenderService.java
@@ -5,15 +5,32 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import pl.mszyb.med_facility.entity.ConfirmationToken;
+import pl.mszyb.med_facility.entity.User;
 
 @Service
 @AllArgsConstructor
 public class EmailSenderService {
 
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Async
     public void sendMail(SimpleMailMessage mail){
         javaMailSender.send(mail);
     }
+
+    public SimpleMailMessage createRegistrationMail(User user){
+        ConfirmationToken confirmationToken = new ConfirmationToken();
+        confirmationToken.setUser(user);
+        confirmationTokenService.save(confirmationToken);
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setSubject("MedFacility registration process");
+        mailMessage.setFrom("meedfaacility@gmail.com");
+        mailMessage.setText("You are almost there! Please click this link to confirm your email address and finish registration process "
+                + " http://localhost:8080/sign_in/confirmation?token=" + confirmationToken.getConfirmationToken());
+        return mailMessage;
+    }
+
 }
