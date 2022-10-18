b744af8a2b3867f89d2e7fad478c136d73c908ac
marcinszyb <marcinszyb@gmail.com>
2022-10-18 14:08:37 +0200

    UserSpecServ_Service tests added

---
+++ src/test/java/pl/mszyb/med_facility/service/user_spec_serv/ServAndSpecData.java
@@ -0,0 +1,12 @@
+package pl.mszyb.med_facility.service.user_spec_serv;
+
+public class ServAndSpecData {
+    public static final String SPEC_1 = "some_spec_1";
+    public static final String SPEC_2 = "some_spec_2";
+    public static final String SERVICE_1 = "some_serv_1";
+    public static final String SERVICE_2 = "some_serv_2";
+    public static final String SERVICE_3 = "some_serv_3";
+    public static final String SERVICE_4 = "some_serv_4";
+    public static final String SERVICE_5 = "some_serv_5";
+    public static final String SERVICE_6 = "some_serv_6";
+}
