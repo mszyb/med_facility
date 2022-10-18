fb5c588f6de5c6e72fc35da9c47c403986bc3317
marcinszyb <marcinszyb@gmail.com>
2022-10-18 11:53:32 +0200

    ServiceTypeService tests added

---
+++ src/test/java/pl/mszyb/med_facility/service/ServiceTypeServiceTest.java
@@ -0,0 +1,56 @@
+package pl.mszyb.med_facility.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.mszyb.med_facility.entity.ServiceType;
+import pl.mszyb.med_facility.repository.ServiceTypeRepository;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ServiceTypeServiceTest {
+
+    @Mock
+    ServiceTypeRepository repository;
+    @InjectMocks
+    ServiceTypeService serviceTypeService;
+    private ServiceType service;
+
+    @BeforeEach
+    void setup() {
+        service = new ServiceType();
+        service.setId(1L);
+    }
+
+    @Test
+    void should_activate_service() {
+        given(repository.findById(anyLong())).willReturn(Optional.of(service));
+        service.setActive(false);
+        serviceTypeService.activate(service.getId());
+        assertTrue(service.isActive());
+    }
+
+    @Test
+    void should_deactivate_service() {
+        given(repository.findById(anyLong())).willReturn(Optional.of(service));
+        service.setActive(true);
+        serviceTypeService.deactivate(service.getId());
+        assertFalse(service.isActive());
+    }
+
+    @Test
+    void should_throw_no_such_element_exception() {
+        given(repository.findById(anyLong())).willReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class, () -> serviceTypeService.activate(service.getId()));
+        assertThrows(NoSuchElementException.class, () -> serviceTypeService.deactivate(service.getId()));
+    }
+}
